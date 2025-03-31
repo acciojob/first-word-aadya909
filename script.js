c127b65272e4a3076a181a9f187506548962a24f
aadya909 <145979679+aadya909@users.noreply.github.com>
2025-03-31 12:52:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
 function firstWord(s) {
-  // your code here
+  // Trim leading spaces to find the first actual word
+  s = s.trimStart();
+  
+  // Find the index of the first space
+  let spaceIndex = s.indexOf(' ');
+  
+  // If no space is found, return the entire string
+  if (spaceIndex === -1) {
+    return s;
+  }
+  
+  // Return substring up to the first space
+  return s.substring(0, spaceIndex);
 }
 
 // Do not change the code below
-
 const s = prompt("Enter String:");
 alert(firstWord(s));
+
